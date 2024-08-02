9b06c4048e27f3b984a622bc17d513db15fa2a82
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-08-02 11:43:37 +0200

    [UPD] Header Done (partially)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script>
-import { store } from './store.js'
+import { store } from './store.js';
+
+import  AppHeader  from './components/AppHeader.vue';
+
+
 export default {
+  components: {
+    AppHeader
+  },
   data(){
     return{
       store
@@ -13,9 +20,9 @@ export default {
 </script>
 <template lang="">
   <div>
-    
+      <AppHeader />
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @import './styles/generals.scss';
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,35 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
-    </div>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6">
+                    <img class="logo d-inline-block v-align-middle" src="/img/logo_boolean_zoom.png" alt="Boolean Glossary" />
+                    <h2 class="d-inline-block v-align-middle">Boolean</h2>
+                </div>
+                <div class="col-6">
+                    <input type="text" placeholder="Cerca definizione">
+                </div>
+            </div>
+        </div>
+
+    </header>
 </template>
-<style lang="">
-    
+<style lang="scss" scoped>
+header{
+    color: #fff;
+    .logo{
+        width: 100px;
+    }
+
+    .v-align-middle{
+        vertical-align: middle;
+    }
+
+    input{
+        float: right;
+        margin-top: 40px;
+    }
+}
 </style>
